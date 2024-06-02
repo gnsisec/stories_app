36af09b83d2b7edfeffe377c50e42a138f91cab3
mattivityroom <mattivityroom@gmail.com>
2024-06-02 22:49:31 +0700

    [MVP upload] Gallery

--- app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/upload/UploadActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/upload/UploadActivity.kt
@@ -1,15 +1,52 @@
 package com.dicoding.picodiploma.loginwithanimation.view.upload
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dicoding.picodiploma.loginwithanimation.R
+import com.dicoding.picodiploma.loginwithanimation.databinding.ActivityMainBinding
+import com.dicoding.picodiploma.loginwithanimation.databinding.ActivityUploadBinding
 
 class UploadActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadBinding
+    private var currentImageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bGalery.setOnClickListener {
+            startGallery()
+        }
+    }
+
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.ivPreview.setImageURI(it)
+        }
     }
 }
